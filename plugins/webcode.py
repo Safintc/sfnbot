ff24755be01cbfdba95d99bd9832110204b49687
Safin Thangachan <safintc7899@gmail.com>
2025-09-29 18:24:58 +0530

    Update webcode.py

--- plugins/webcode.py
+++ plugins/webcode.py
@@ -1,13 +1,26 @@
 from aiohttp import web as webserver
 
+# -------------------- Routes --------------------
 routes = webserver.RouteTableDef()
 
+# -------------------- Web App --------------------
 async def bot_run():
-    _app = webserver.Application(client_max_size=30000000)
+    """
+    Returns an aiohttp Application for the bot's web server.
+    Used to keep the Replit instance alive via UptimeRobot.
+    """
+    _app = webserver.Application(client_max_size=30_000_000)
     _app.add_routes(routes)
     return _app
-  
+
+# -------------------- Root Route --------------------
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return webserver.json_response("Goutham Josh KuttuBot Web Supported . . . !  This is a preview of WeB . . .! ! !")
- 
+    """
+    Handles GET requests to the root URL.
+    Returns a simple JSON message for UptimeRobot.
+    """
+    return webserver.json_response({
+        "status": "online",
+        "message": "Safin SfnBot Web Supported! This is a preview of Web."
+    })
