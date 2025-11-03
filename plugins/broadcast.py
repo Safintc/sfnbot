82871f704ad0aab4834052ee6046566ac407ad19
Safin Thangachan <safintc7899@gmail.com>
2025-11-03 17:42:48 +0530

    Refactor broadcast function for clarity and updates

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -8,7 +8,7 @@ from utils import broadcast_messages
 import asyncio
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
+# https://t.me/sfn_moviestore
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
@@ -39,4 +39,5 @@ async def verupikkals(bot, message):
         if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
